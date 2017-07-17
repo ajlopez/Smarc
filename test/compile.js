8d14848de90390317a820d8ed45ba5166f966957
ajlopez <webmaster@ajlopez.com>
2017-07-17 18:18:37 -0300

    Compile contract and create instance

--- test/compile.js
+++ test/compile.js
@@ -2,9 +2,21 @@
 var smarc = require('..');
 
 exports['compiles simple contract'] = function (test) {
-	var compiled = smarc.compile('method("increment", function (x) { return x + 1 }');
+	var compiled = smarc.compile('method("increment", function (x) { return x + 1 })');
 	
 	test.ok(compiled);
 	test.equal(typeof compiled, 'object');
 };
+
+exports['create instance'] = function (test) {
+	var compiled = smarc.compile('method("increment", function (x) { return x + 1 })');
+	var instance = compiled.instance();
+	
+	test.ok(instance);
+	test.equal(typeof instance, 'object');
+	test.equal(instance.increment(1), 2);
+	test.equal(instance.increment(41), 42);
+};
+
+
 
