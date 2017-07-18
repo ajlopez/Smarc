74b994ed6bb5ee3a3aa4960ab89ab2b827565ca2
ajlopez <webmaster@ajlopez.com>
2017-07-18 13:44:14 -0300

    Accessing storage

--- test/compile.js
+++ test/compile.js
@@ -29,12 +29,22 @@ exports['sealed instance'] = function (test) {
 
 exports['create instance calling initialize'] = function (test) {
 	var compiled = smarc.compile('public("initialize", function (msg) { this.message = msg; }); public("message");');
-	var instance = compiled.instance("hello");
+	var instance = compiled.instance(["hello"]);
 	
 	test.ok(instance);
 	test.equal(typeof instance, 'object');
 	test.equal(instance.message, "hello");
 };
 
+exports['accessing storage'] = function (test) {
+	var compiled = smarc.compile('public("initialize", function (msg) { this.message = msg; }); public("message");');
+	var storage = {};
+	var instance = compiled.instance(["hello"], storage);
+	
+	test.ok(instance);
+	test.equal(typeof instance, 'object');
+	test.equal(instance.message, "hello");
+	test.equal(storage.message, "hello");
+};
 
 
