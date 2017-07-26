c34b35ccc5d4a7c137b182a3777c7b9b4e80baa1
ajlopez <webmaster@ajlopez.com>
2017-07-26 10:46:26 -0300

    Remove public function

--- test/compile.js
+++ test/compile.js
@@ -1,28 +1,11 @@
 
 var smarc = require('..');
 
-exports['compiles simple contract'] = function (test) {
-	var compiled = smarc.compile('public("increment", function (x) { return x + 1; })');
-	
-	test.ok(compiled);
-	test.equal(typeof compiled, 'object');
-};
-
 exports['compiles simple contract with definition'] = function (test) {
 	var compiled = smarc.compile('function increment(x) { return x + 1; }; contract({ methods: { public: { increment: increment }}});');
 	
 	test.ok(compiled);
 	test.equal(typeof compiled, 'object');
-};
-
-exports['create instance'] = function (test) {
-	var compiled = smarc.compile('public("increment", function (x) { return x + 1; })');
-	var instance = compiled.instance();
-	
-	test.ok(instance);
-	test.equal(typeof instance, 'object');
-	test.equal(instance.increment(1), 2);
-	test.equal(instance.increment(41), 42);
 };
 
 exports['create instance using contract with definition'] = function (test) {
@@ -36,7 +19,7 @@ exports['create instance using contract with definition'] = function (test) {
 };
 
 exports['sealed instance'] = function (test) {
-	var compiled = smarc.compile('public("increment", function (x) { return x + 1; })');
+	var compiled = smarc.compile('function increment(x) { return x + 1; }; contract({ methods: { public: { increment: increment }}});');
 	var instance = compiled.instance();
 	
 	test.ok(instance);
